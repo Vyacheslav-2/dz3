86fa80f02be5f89f10b0a7355d925269bfd0c94e
Vyacheslav <vip041961@yandex.ru>
2023-10-25 23:34:49 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,65 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+
+class Calculator {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        while (true) {
+            System.out.print("Введите первое число: ");
+            int num1 = scanner.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.print("Введите второе число: ");
+            int num2 = scanner.nextInt();
+
+            System.out.println("Выберите операцию:");
+            System.out.println("1. Сложение");
+            System.out.println("2. Вычитание");
+            System.out.println("3. Умножение");
+            System.out.println("4. Деление");
+            System.out.println("0. Выход");
+
+            int operation = scanner.nextInt();
+            int result;
+
+            switch (operation) {
+                case 1:
+                    result = add(num1, num2);
+                    break;
+                case 2:
+                    result = subtract(num1, num2);
+                    break;
+                case 3:
+                    result = multiply(num1, num2);
+                    break;
+                case 4:
+                    result = divide(num1, num2);
+                    break;
+                case 0:
+                    System.out.println("Программа завершена.");
+                    return;
+                default:
+                    System.out.println("Неверная операция. Повторите ввод.");
+                    continue;
+            }
+
+            System.out.println("Результат: " + result);
+            System.out.println();
         }
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
     }
 }
